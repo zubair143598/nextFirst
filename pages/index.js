0db6c84abfe62adb5224fb17d102bce468cb8e56
M Zubair <md.zubair143598@gmail.com>
2022-11-29 18:39:12 +0500

    inittttt

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,12 @@ import Footer from '../Component/Layout/Footer'
 import Header from '../Component/Layout/Header'
 import styles from '../styles/Home.module.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Hero from '../Component/Element/Hero'
 export default function Home() {
   return (
     <>
     <Header/>
+    <Hero/>
     <Footer/>
     </>
   )
